aa6016747788b523fe3b3c6117c132f7743b195c
AymaneBOUJELLABA <aymanoosse.b.j@gmail.com>
2020-07-02 02:09:09 +0100

    updated api and added usercontroller

--- ProjetCovid19/backend/routes/api.php
+++ ProjetCovid19/backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\User;
@@ -34,8 +35,11 @@ Route::middleware('auth:sanctum')->group(
     }
 );
 
+
+//les routes pour les informations de chaque utilisateur
 Route::middleware('auth:sanctum')->group(
-    function () {
+    function ()
+    {
         Route::get('infos', 'InfoController@index');
         Route::get('info/{id}', 'InfoController@show');
         Route::post('info/{id}', 'InfoController@store');
@@ -45,100 +49,21 @@ Route::middleware('auth:sanctum')->group(
 );
 
 
-//For the users ( customers)
+//les routes pour controller l'authentification de l'utilisateur
+Route::middleware('auth:sanctum')->group(
+    function ()
+    {
+        Route::get('/user', 'UserController@currentUser');
+        Route::get('/users',"UserController@index");
 
-//get the user I'm using to log in
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-//get all the  users if I'm logged in
-Route::middleware('auth:sanctum')->get('/users', function () {
-    return User::all();
-});
-//delete the user
-Route::middleware('auth:sanctum')->delete('user/delete/{id}', function ($id) {
-    $user = User::findOrFail($id);
-    $user->delete();
-    return response('User deleted', 200);
-});
-
-
-
-
-
-//update the user
-Route::middleware('auth:sanctum')->post('/user/update/{id}', function (Request $request, $id) {
-
-    $request->validate([
-        'name' => 'required|string|max:255',
-        'email' => 'required|string|email|max:255|unique:users',
-        'password' => 'required|string|min:8'
-    ]);
-    $user = User::findOrFail($id);
-    $data = $request->all();
-    $user->name = $data['name'];
-    $user->email = $data['email'];
-    $user->password = Hash::make($data['password']);
-    $user->save();
-    return response('user Updated', 200);
-});
-
-
-//logout if I'm logged in
-Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
-    $request->user()->tokens()->delete();
-    return response('Logged out', 200);
-});
-
-
-//register a new user
-
-Route::post('/register', function (Request $request) {
-    $request->validate([
-        'name' => 'required|string|max:255',
-        'email' => 'required|string|email|max:255|unique:users',
-        'password' => 'required|string|min:8',
-        'device_name' => 'required'
-    ]);
-
-    $user = User::create(
-        [
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]
-    );
-
-    $token = $user->createToken($request->device_name)->plainTextToken;
-
-    $response = [
-        'user' => $user,
-        'token' => $token
-    ];
-    return response($response, 201);
-});
-
-//the login
-Route::post('/login', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required'
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (!$user || !Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect!.'],
-        ]);
+        //pour l'admin
+        Route::post('/admin/register', 'UserController@registerAdmin')->withoutMiddleware('auth:sanctum');
+        //pour utilisateur simple
+        Route::post('/user/logout','UserController@logout');
+        Route::post('/user/login','UserController@login')->withoutMiddleware('auth:sanctum');
+        Route::post('/user/register', 'UserController@register')->withoutMiddleware('auth:sanctum');
+        
+        Route::put('/user/update/{id}', 'UserController@update');
+        Route::delete('user/delete/{id}','UserController@delete');
     }
-
-    $token = $user->createToken($request->device_name)->plainTextToken;
-
-    $response = [
-        'user' => $user,
-        'token' => $token
-    ];
-    return response($response, 201);
-});
+);
